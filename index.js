cf4f969c2efdef46dca36df47a2ef72743373304
jdmedlock <jdmedlock@gmail.com>
2019-12-01 09:40:27 -0600

    Feature: Add new utterances and intents
    Add new utterances and intents to train the NLG
    
    Resolves: N/a
    See also: N/a

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const { NlpManager } = require('node-nlp');
  
 const manager = new NlpManager({ languages: ['en', 'hi'] });
-// Adds the utterances and intents for the NLP
+// Adds the utterances and intents for the Natural Language Processor (NLP)
+manager.addDocument('en', 'ask question', 'ask.question');
 manager.addDocument('en', 'how can I get technical help', 'ask.question');
-manager.addDocument('hi', 'mujhe takaneekee madad kaise mil sakatee hai');
+manager.addDocument('hi', 'mujhe takaneekee madad kaise mil sakatee hai', 'ask.question');
 manager.addDocument('en', 'I need technical help', 'ask.question');
 manager.addDocument('en', 'can anyone help me', 'ask.question');
 manager.addDocument('en', 'where can I get help', 'ask.doc');
@@ -12,7 +13,7 @@ manager.addDocument('en', 'display the voyage schedule', 'schedule.voyage');
 manager.addDocument('en', 'list the voyage schedule', 'schedule.voyage');
 manager.addDocument('en', 'show the voyage schedule', 'schedule.voyage');
  
-// Train also the NLG
+// Train the Natural Language Generator (NLG)
 manager.addAnswer('en', 'ask.doc', 'https://docs.chingu.io');
 manager.addAnswer('en', 'ask.question', 'https://chingu.docs.chingu.io/about/askhelp');
 manager.addAnswer('hi', 'ask.question', 'https://chingu.docs.chingu.io/about/askhelp');
@@ -23,6 +24,6 @@ manager.addAnswer('en', 'schedule.voyage', 'https://chingu.docs.chingu.io/intro/
     await manager.train();
     manager.save();
     //const response = await manager.process('en', 'what is the voyage schedule');
-    const response = await manager.process('I need help');
+    const response = await manager.process('I have a question');
     console.log(response);
 })();
